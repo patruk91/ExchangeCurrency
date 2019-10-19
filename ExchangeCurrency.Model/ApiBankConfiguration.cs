797257818b3062737b80c131fdc755b604ef7120
Patryk Lichwala <lichwalapatryk@gmail.com>
2019-10-19 15:19:59 +0200

    Add links to configure URI to external API

---
+++ ExchangeCurrency.Model/ApiBankConfiguration.cs
@@ -0,0 +1,8 @@
+﻿namespace ExchangeCurrency.Model
+{
+    public static class ApiBankConfiguration
+    {
+        public const string UriStringToNbpApi = @"http://api.nbp.pl/";
+        public const string RequestUriToGetCurrentExchangeRates = @"api/exchangerates/tables/A/";
+    }
+}
