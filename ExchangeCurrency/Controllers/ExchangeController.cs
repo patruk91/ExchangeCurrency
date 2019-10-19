3fc858e245b2249a1e2f7a1b53e68e43f609fddb
Patryk Lichwala <lichwalapatryk@gmail.com>
2019-10-19 08:10:24 +0200

    Add template structure for web api

---
+++ ExchangeCurrency/Controllers/ExchangeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ExchangeCurrency.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ExchangeController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            return "value1";
+        }
+    }
+}
