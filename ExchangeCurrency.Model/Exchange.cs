12a3ee69629f5bf5acde25e23302d44a2a4fbd0d
Patryk Lichwala <lichwalapatryk@gmail.com>
2019-10-19 15:16:52 +0200

    Get current exchange rates

---
+++ ExchangeCurrency.Model/Exchange.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+
+namespace ExchangeCurrency.Model
+{
+    public class Exchange : IExchange
+    {
+        public async Task<string> GetCurrentExchangeRates(string uriString, string requestUri)
+        {
+            Task<string> exchangeRates;
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(uriString);
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                var response = await client.GetAsync(requestUri);
+                if (response.IsSuccessStatusCode)
+                {
+                    exchangeRates = response.Content.ReadAsStringAsync();
+                }
+                else
+                {
+                    return response.StatusCode.ToString();
+                }
+            }
+            return exchangeRates.Result;
+        }
+    }
+
+
+}
