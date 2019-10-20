ccf17545a52f180e93d7c5968e55a2f31e04cd9f
Patryk Lichwala <lichwalapatryk@gmail.com>
2019-10-20 08:35:50 +0200

    Add URI link to get exchage rate

--- ExchangeCurrency.Model/ApiBankConfiguration.cs
+++ ExchangeCurrency.Model/ApiBankConfiguration.cs
@@ -4,5 +4,6 @@
     {
         public const string UriStringToNbpApi = @"http://api.nbp.pl/";
         public const string RequestUriToGetCurrentExchangeRates = @"api/exchangerates/tables/A/";
+        public const string RequestUriToGetExchangeRate = @"api/exchangerates/rates/";
     }
 }
