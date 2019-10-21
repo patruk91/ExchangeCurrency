b21774ea13baa8a69de4abdaf7748777175fe055
Patryk Lichwala <lichwalapatryk@gmail.com>
2019-10-21 08:28:32 +0200

    Add database configurations for currency and details

---
+++ ExchangeCurrency.AccessLayer/entityConfiguration/CurrencyDetailsEntityConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using ExchangeCurrency.Model.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ExchangeCurrency.AccessLayer.entityConfiguration
+{
+    public class CurrencyDetailsEntityConfiguration : IEntityTypeConfiguration<CurrencyDetails>
+    {
+        public void Configure(EntityTypeBuilder<CurrencyDetails> builder)
+        {
+            builder
+                .Property(i => i.CurrencyDetailsId)
+                .ValueGeneratedOnAdd();
+
+        }
+    }
+}
---
+++ ExchangeCurrency.AccessLayer/entityConfiguration/CurrencyEntityConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using ExchangeCurrency.Model.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ExchangeCurrency.AccessLayer.entityConfiguration
+{
+    public class CurrencyEntityConfiguration : IEntityTypeConfiguration<Currency>
+    {
+        public void Configure(EntityTypeBuilder<Currency> builder)
+        {
+            builder
+                .Property(i => i.CurrencyId)
+                .ValueGeneratedOnAdd();
+
+            builder.HasOne<CurrencyDetails>(d => d.Details)
+                .WithOne(c => c.Currency)
+                .HasForeignKey<CurrencyDetails>(d => d.CurrencyRef);
+
+        }
+    }
+}
