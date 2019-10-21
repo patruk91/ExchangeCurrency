a8e2931096e42a0e5ac035e18c5f49a65698762a
Patryk Lichwala <lichwalapatryk@gmail.com>
2019-10-21 08:22:15 +0200

    Add method to save currency to database

---
+++ ExchangeCurrency.AccessLayer/dao/ICurrencyDao.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using ExchangeCurrency.Model.Models;
+
+namespace ExchangeCurrency.AccessLayer.dao
+{
+    public interface ICurrencyDao
+    {
+        Task AddCurrency(Currency currency, ExchangeDbEntities context);
+    }
+}
