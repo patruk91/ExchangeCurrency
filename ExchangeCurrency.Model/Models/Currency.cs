3eb8c1d8488bf50da129012eebbb64572b83fff3
Patryk Lichwala <lichwalapatryk@gmail.com>
2019-10-21 08:25:05 +0200

    Add classes to model currency

---
+++ ExchangeCurrency.Model/Models/Currency.cs
@@ -0,0 +1,20 @@
+﻿namespace ExchangeCurrency.Model.Models
+{
+    public class Currency
+    {
+        public int CurrencyId { get; set; }
+        public string Code { get; set; }
+        public decimal ExchangeRate { get; set; }
+
+        public CurrencyDetails Details { get; set; }
+
+        public Currency() { }
+
+        public Currency(string code, decimal exchangeRate)
+        {
+            Code = code;
+            ExchangeRate = exchangeRate;
+        }
+
+    }
+}
