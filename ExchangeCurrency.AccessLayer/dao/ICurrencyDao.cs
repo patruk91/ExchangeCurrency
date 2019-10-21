541238f2ae44846829a221bb5d5691af9c18fe3d
Patryk Lichwala <lichwalapatryk@gmail.com>
2019-10-21 21:10:23 +0200

    Get Currency from database

---
+++ ExchangeCurrency.AccessLayer/dao/ICurrencyDao.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using ExchangeCurrency.ModelExchangeCurrency.Models;
+
+namespace ExchangeCurrency.AccessLayer.dao
+{
+    public interface ICurrencyDao
+    {
+        Currency GetCurrency(string currencyData,
+                            ExchangeDbEntities context,
+                            Dictionary<string, int> codesForExchangeRates);
+    }
+}
---
+++ ExchangeCurrency.AccessLayer/dao/sql/CurrencySql.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using ExchangeCurrency.ModelExchangeCurrency.Models;
+
+namespace ExchangeCurrency.AccessLayer.dao.sql
+{
+    public class CurrencySql : ICurrencyDao
+    {
+        public Currency GetCurrency(string currencyData,
+                                    ExchangeDbEntities context,
+                                    Dictionary<string, int> codesForExchangeRates)
+        {
+            return context.Currency.Find(codesForExchangeRates[currencyData]);
+        }
+    }
+}
