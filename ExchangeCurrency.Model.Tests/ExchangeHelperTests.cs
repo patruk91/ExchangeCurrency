ddacfc7d02b070d8d2991033e03e1c98fd11075e
Patryk Lichwala <lichwalapatryk@gmail.com>
2019-10-23 08:28:12 +0200

    Add project to test Model funcionality

---
+++ ExchangeCurrency.Model.Tests/ExchangeHelperTests.cs
@@ -0,0 +1,14 @@
+using System;
+using ExchangeCurrency.Model.ExchangeCurrency;
+using Newtonsoft.Json.Linq;
+using NUnit.Framework;
+
+namespace ExchangeCurrency.Model.Tests
+{
+    public class ExchangeHelperTests
+    {
+
+
+
+    }
+}
